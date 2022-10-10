fbbff2986f8a79620b6a18f45a94f4ba9dd9e33e
Vinicius7Codeby <viniciusgabriel.pb@gmail.com>
2022-10-10 19:15:44 -0300

    feat: :art: add customer prisma infra base

---
+++ server/src/modules/customers/infra/prisma/entities/Customer.ts
@@ -0,0 +1,11 @@
+export class Customer {
+  id: string
+
+  code: string
+
+  name: string
+
+  created_at: Date
+
+  updated_ad: Date
+}
---
+++ server/src/modules/customers/infra/prisma/repositorires/CustomerRepository.ts
@@ -0,0 +1,5 @@
+import { ICustomerRepository } from '@modules/customers/repositories/ICustomerRepository'
+
+export class CustomerRepository implements ICustomerRepository {
+
+}
