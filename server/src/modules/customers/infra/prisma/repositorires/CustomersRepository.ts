3af322da9ce7c0c77bfa11321d16d06c7e3be21e
Vinicius7Codeby <viniciusgabriel.pb@gmail.com>
2022-10-11 19:14:14 -0300

    feat: :art: implement create and findById methods to customers repository

--- server/src/modules/customers/infra/prisma/repositorires/CustomersRepository.ts
+++ server/src/modules/customers/infra/prisma/repositorires/CustomersRepository.ts
@@ -1,9 +1,22 @@
 import { ICreateCustomer } from '@modules/customers/dtos/ICreateCustomer';
 import { ICustomersRepository } from '@modules/customers/repositories/ICustomersRepository'
-import { Customer } from '@modules/customers/infra/prisma/entities/Customer';
+import { Customer } from '../entities/Customer';
+import { AppRepository } from '@shared/infra/prisma/repositories/AppRepository';
 
-export class CustomersRepository implements ICustomersRepository {
-  public async create(data: ICreateCustomer): Promise<Customer> {
-    throw new Error('Method not implemented.');
+export class CustomersRepository extends AppRepository implements ICustomersRepository {
+  public async findByCode(code: string): Promise<Customer | null> {
+    const findedCustomer = await this.client.customers.findFirst({
+      where: { code }
+    })
+
+    return findedCustomer
+  }
+
+  public async create({ code, name }: ICreateCustomer): Promise<Customer> {
+    const createdCustomer = await this.client.customers.create({
+      data: { code, name }
+    })
+
+    return createdCustomer
   }
 }
