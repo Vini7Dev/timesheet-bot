7d939511ca4cda8b5efe55fc9754e3d988f192ec
Vinicius7Codeby <viniciusgabriel.pb@gmail.com>
2022-10-14 17:35:56 -0300

    feat: :art: creation of Marking DTOs

---
+++ server/src/modules/markings/dtos/ICreateMarkingDTO.ts
@@ -0,0 +1,16 @@
+enum WorkClass {
+  PRODUCTION,
+  ABSENCE
+}
+
+export interface ICreateMarkingDTO {
+  description: string
+  date: string
+  start_time: string
+  finish_time: string
+  start_interval_time?: string
+  finish_interval_time?: string
+  work_class: WorkClass
+  user_id: string
+  project_id: string
+}
---
+++ server/src/modules/markings/dtos/IUpdateMarkingDTO.ts
@@ -0,0 +1,16 @@
+enum WorkClass {
+  PRODUCTION,
+  ABSENCE
+}
+
+export interface IUpdateMarkingDTO {
+  id: string
+  description?: string
+  date?: string
+  start_time?: string
+  finish_time?: string
+  start_interval_time?: string
+  finish_interval_time?: string
+  work_class?: WorkClass
+  project_id?: string
+}
