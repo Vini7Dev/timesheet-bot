48048d486b81c16abe186809e752e8a8df4120b0
Vinicius7Codeby <viniciusgabriel.pb@gmail.com>
2022-10-14 20:22:13 -0300

    feat: :art: development of CreateMarkingService method

--- server/src/modules/markings/services/CreateMarkingService.ts
+++ server/src/modules/markings/services/CreateMarkingService.ts
@@ -1,5 +1,7 @@
 import { IProjectsRepository } from '@modules/projects/repositories/IProjectsRepository';
 import { IUsersRepository } from '@modules/users/repositories/IUsersRepository';
+import { AppError } from '@shared/errors/AppError';
+import { filterNumberBetweenInterval } from '@utils/filterNumberBetweenInterval';
 import { Marking } from '../infra/prisma/entities/Marking';
 import { IMarkingsRepository } from '../repositories/IMarkingsRepository'
 
@@ -29,7 +31,102 @@ export class CreateMarkingService {
     private projectsRepository: IProjectsRepository,
   ) {}
 
-  public async execute(data: IServiceProps): Promise<Marking> {
-    throw new Error('Method not implemented.')
+  public async execute({
+    description,
+    date,
+    start_time,
+    finish_time,
+    start_interval_time,
+    finish_interval_time,
+    work_class,
+    project_id,
+    authenticatedUserId,
+  }: IServiceProps): Promise<Marking> {
+    const startNumber = parseInt(start_time.replace(':', ''))
+    const finishNumber = parseInt(finish_time.replace(':', ''))
+
+    if(start_interval_time || finish_interval_time) {
+      if(!start_interval_time || !finish_interval_time) {
+        throw new AppError('Only one of interval times was received!')
+      }
+
+      const startIntervalNumber = parseInt(start_interval_time.replace(':', ''))
+      const finishIntervalNumber = parseInt(finish_interval_time.replace(':', ''))
+
+      const intervalTimeIsOnStartAndFinishRange = filterNumberBetweenInterval({
+        startNumberInterval: startNumber,
+        finishNumberInterval: finishNumber,
+        numbersToVerify: [startIntervalNumber, finishIntervalNumber],
+      }).length === 2
+
+      if(!intervalTimeIsOnStartAndFinishRange) {
+        throw new AppError('Interval times are outside of the start and finish times!')
+      }
+    }
+
+    const userToRefer = await this.usersRepository.findById(authenticatedUserId)
+    if (!userToRefer) {
+      throw new AppError('User not found!', 404)
+    }
+
+    const projectToRefer = await this.projectsRepository.findById(project_id)
+    if (!projectToRefer) {
+      throw new AppError('Project not found!', 404)
+    }
+
+    let page = 0
+    const perPage = 1000
+    const markingsOfDate: Marking[] = []
+
+    while (true) {
+      const markingsList = await this.markingsRepository.list({
+        page,
+        perPage,
+        date,
+      })
+
+      if(markingsList.length === 0) {
+        break
+      }
+
+      markingsOfDate.push(...markingsList)
+      page += 1
+    }
+
+    const parallelMarking = markingsOfDate.find(markingToCompare => {
+      const {
+        start_time: compareStartTime,
+        finish_time: compareFinishTime
+      } = markingToCompare
+
+      const compareStartNumber = parseInt(compareStartTime.replace(':', ''))
+      const compareFinishNumber = parseInt(compareFinishTime.replace(':', ''))
+
+      const isParallel = filterNumberBetweenInterval({
+        startNumberInterval: compareStartNumber,
+        finishNumberInterval: compareFinishNumber,
+        numbersToVerify: [startNumber, finishNumber],
+      }).length !== 0
+
+      return isParallel
+    })
+
+    if (parallelMarking) {
+      throw new AppError('Another task already exists in parallel date and time!')
+    }
+
+    const createdMarking = await this.markingsRepository.create({
+      description,
+      date,
+      start_time,
+      finish_time,
+      start_interval_time,
+      finish_interval_time,
+      work_class,
+      project_id,
+      user_id: userToRefer.id,
+    })
+
+    return createdMarking
   }
 }
