ae31ebdd12bb96ae5193243c97350e79a9468951
Vinicius7Codeby <viniciusgabriel.pb@gmail.com>
2022-10-15 00:10:06 -0300

    feat: :art: add markings implementations on resolvers

--- server/src/modules/markings/infra/apollo/schemas/markingsResolvers.ts
+++ server/src/modules/markings/infra/apollo/schemas/markingsResolvers.ts
@@ -1,6 +1,10 @@
+import { marking } from './resolvers/markingResolver'
+import { markings } from './resolvers/markingsResolver'
 import { createMarking } from './resolvers/createMarkingResolver'
+import { updateMarking } from './resolvers/updateMarkingResolver'
+import { deleteMarking } from './resolvers/deleteMarkingResolver'
 
 export const markingsResolvers = {
-  Query: {},
-  Mutation: { createMarking },
+  Query: { marking, markings },
+  Mutation: { createMarking, updateMarking, deleteMarking },
 }
