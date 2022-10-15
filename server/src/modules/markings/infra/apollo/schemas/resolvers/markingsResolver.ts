f2ee3a0dea2e4603d2a988441fd5c9f08c8a3509
Vinicius7Codeby <viniciusgabriel.pb@gmail.com>
2022-10-15 00:09:40 -0300

    feat: :art: implementation of ListMarkingService

---
+++ server/src/modules/markings/infra/apollo/schemas/resolvers/markingsResolver.ts
@@ -0,0 +1,29 @@
+import { container } from 'tsyringe'
+
+import { IAppContext } from '@shared/infra/apollo/context'
+import { ensureAuthenticated } from '@utils/ensureAuthenticated'
+import { ListMarkingsService } from '@modules/markings/services/ListMarkingsService'
+
+interface IApiFiltersInput {
+  data: {
+    page?: number
+    perPage?: number
+  }
+}
+
+export const markings = async (
+  _: any,
+  { data: { page, perPage } }: IApiFiltersInput,
+  ctx: IAppContext,
+) => {
+  ensureAuthenticated(ctx)
+
+  const listMarkingsService = container.resolve(ListMarkingsService)
+
+  const markingsList = await listMarkingsService.execute({
+    page,
+    perPage,
+  })
+
+  return markingsList
+}
