ea805c3ac1872cae468644a5cba2e03950e7a820
Vinicius7Codeby <viniciusgabriel.pb@gmail.com>
2023-01-13 17:36:57 -0300

    feat: :art: marking owner validation before delete

--- server/src/modules/markings/services/DeleteMarkingService.spec.ts
+++ server/src/modules/markings/services/DeleteMarkingService.spec.ts
@@ -3,22 +3,34 @@ import 'reflect-metadata'
 import { WorkClass } from '@prisma/client'
 
 import { AppError } from '@shared/errors/AppError'
+import { IUsersRepository } from '@modules/users/repositories/IUsersRepository'
+import { FakeUsersRepository } from '@modules/users/repositories/fakes/FakeUsersRepository'
 import { FakeMarkingsRepository } from '../repositories/fakes/FakeMarkingsRepository'
 import { IMarkingsRepository } from '../repositories/IMarkingsRepository'
 import { DeleteMarkingService } from './DeleteMarkingService'
 
 let markingsRepository: IMarkingsRepository
+let usersRepository: IUsersRepository
 let deleteMarkingService: DeleteMarkingService
 
 describe('DeleteMarkingService', () => {
   beforeEach(() => {
     markingsRepository = new FakeMarkingsRepository()
+    usersRepository=  new FakeUsersRepository()
     deleteMarkingService = new DeleteMarkingService(
       markingsRepository,
+      usersRepository,
     )
   })
 
   it('should be able to delete marking', async () => {
+    const authenticatedUser = await usersRepository.create({
+      name: 'Jhon Doe',
+      email: 'jhondoe@mail.com',
+      username: 'jhon.doe',
+      password: 'jhon123',
+    })
+
     const createdMarking = await markingsRepository.create({
       description: 'Description Example',
       date: '01/01/2022',
@@ -28,10 +40,11 @@ describe('DeleteMarkingService', () => {
       finish_interval_time: '11:00',
       work_class: WorkClass.PRODUCTION,
       project_id: 'any-project-id',
-      user_id: 'any-user-id'
+      user_id: authenticatedUser.id
     })
 
     const result = await deleteMarkingService.execute({
+      authenticatedUserId: authenticatedUser.id,
       marking_id: createdMarking.id,
     })
 
@@ -39,6 +52,13 @@ describe('DeleteMarkingService', () => {
   })
 
   it ('should be able to delete marking with options', async () => {
+    const authenticatedUser = await usersRepository.create({
+      name: 'Jhon Doe',
+      email: 'jhondoe@mail.com',
+      username: 'jhon.doe',
+      password: 'jhon123',
+    })
+
     const createdMarking = await markingsRepository.create({
       description: 'Description Example',
       date: '01/01/2022',
@@ -48,10 +68,11 @@ describe('DeleteMarkingService', () => {
       finish_interval_time: '11:00',
       work_class: WorkClass.PRODUCTION,
       project_id: 'any-project-id',
-      user_id: 'any-user-id'
+      user_id: authenticatedUser.id
     })
 
     const result = await deleteMarkingService.execute({
+      authenticatedUserId: authenticatedUser.id,
       marking_id: createdMarking.id,
       options: { clearTimesheetId: true }
     })
@@ -60,14 +81,74 @@ describe('DeleteMarkingService', () => {
   })
 
   it('should not be able to delete a non-existent marking', async () => {
+    const authenticatedUser = await usersRepository.create({
+      name: 'Jhon Doe',
+      email: 'jhondoe@mail.com',
+      username: 'jhon.doe',
+      password: 'jhon123',
+    })
+
     await expect(
       deleteMarkingService.execute({
+        authenticatedUserId: authenticatedUser.id,
         marking_id: 'invalid-marking-id',
       })
     ).rejects.toEqual(new AppError('Marking not found!', 404))
   })
 
+  it('should not be able to delete marking with a non-existent user', async () => {
+    const createdMarking = await markingsRepository.create({
+      description: 'Description Example',
+      date: '01/01/2022',
+      start_time: '09:00',
+      finish_time: '12:00',
+      start_interval_time: '10:00',
+      finish_interval_time: '11:00',
+      work_class: WorkClass.PRODUCTION,
+      project_id: 'any-project-id',
+      user_id: 'invalid-user-id'
+    })
+
+    await expect(deleteMarkingService.execute({
+      authenticatedUserId: 'invalid-user-id',
+      marking_id: createdMarking.id
+    })).rejects.toEqual(new AppError('User not found!', 404))
+  })
+
+  it ("should not be able to delete different user's marking", async () => {
+    const differentUser = await usersRepository.create({
+      name: 'Jhon Doe',
+      email: 'jhondoe@mail.com',
+      username: 'jhon.doe',
+      password: 'jhon123',
+    })
+
+    const createdMarking = await markingsRepository.create({
+      description: 'Description Example',
+      date: '01/01/2022',
+      start_time: '09:00',
+      finish_time: '12:00',
+      start_interval_time: '10:00',
+      finish_interval_time: '11:00',
+      work_class: WorkClass.PRODUCTION,
+      project_id: 'any-project-id',
+      user_id: 'any-user-id'
+    })
+
+    await expect(deleteMarkingService.execute({
+      authenticatedUserId: differentUser.id,
+      marking_id: createdMarking.id
+    })).rejects.toEqual(new AppError('You do not have permission to delete this marking!', 403))
+  })
+
   it('should not be able to delete marking if it has status sending in timesheet', async () => {
+    const authenticatedUser = await usersRepository.create({
+      name: 'Jhon Doe',
+      email: 'jhondoe@mail.com',
+      username: 'jhon.doe',
+      password: 'jhon123',
+    })
+
     const createdMarkingWithSendingStatus = await markingsRepository.create({
       description: 'Description Example',
       date: '01/01/2022',
@@ -84,6 +165,7 @@ describe('DeleteMarkingService', () => {
 
     await expect(
       deleteMarkingService.execute({
+        authenticatedUserId: authenticatedUser.id,
         marking_id: createdMarkingWithSendingStatus.id,
       })
     ).rejects.toEqual(new AppError('This marking is being processed in the timesheet'))
--- server/src/modules/markings/services/DeleteMarkingService.ts
+++ server/src/modules/markings/services/DeleteMarkingService.ts
@@ -2,8 +2,10 @@ import { inject, injectable } from 'tsyringe'
 
 import { AppError } from '@shared/errors/AppError'
 import { IMarkingsRepository } from '../repositories/IMarkingsRepository'
+import { IUsersRepository } from '@modules/users/repositories/IUsersRepository'
 
 interface IServiceProps {
+  authenticatedUserId: string
   marking_id: string
   options?: {
     clearTimesheetId?: boolean
@@ -14,18 +16,33 @@ interface IServiceProps {
 export class DeleteMarkingService {
   constructor (
     @inject('MarkingsRepository')
-    private markingsRepository: IMarkingsRepository
+    private markingsRepository: IMarkingsRepository,
+
+    @inject('UsersRepository')
+    private usersRepository: IUsersRepository
   ) {}
 
-  public async execute({ marking_id, options }: IServiceProps): Promise<string> {
+  public async execute({
+    authenticatedUserId,
+    marking_id,
+    options
+  }: IServiceProps): Promise<string> {
     const markingToDelete = await this.markingsRepository.findById(marking_id)
-
     if (!markingToDelete) {
       throw new AppError('Marking not found!', 404)
     }
 
     if (markingToDelete.on_timesheet_status === 'SENDING') {
       throw new AppError('This marking is being processed in the timesheet')
+    }
+
+    if (markingToDelete.user_id !== authenticatedUserId) {
+      throw new AppError('You do not have permission to delete this marking!', 403)
+    }
+
+    const authenticatedUser = await this.usersRepository.findById(authenticatedUserId)
+    if (!authenticatedUser) {
+      throw new AppError('User not found!', 404)
     }
 
     const projectIdDeleted = await this.markingsRepository.delete(marking_id, {
